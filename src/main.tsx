eb999d56c5ff8b3a5a6cb721c1f058fb5f835dd4
ha-ayoub <hafdi.ayoub@gmail.com>
2025-10-22 23:07:44 +0200

    Add images for every games

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import './styles/variables.css'
+import './styles/Index.css'
+import './styles/Variables.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
